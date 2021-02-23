c5c1468a3b26823d7672f682487f16b71ca2e092
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2021-02-23 08:19:33 +0530

    online class

--- app/Http/Controllers/api/TestController.php
+++ app/Http/Controllers/api/TestController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Model\Test;
+use App\Model\TestResult;
 use App\Model\School;
+use App\Model\Student;
+use App\Model\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    public function student(Request $request, School $school)
+    {
+        $section = $request->user();
+        $student = Student::with(['sectionName'])->where('uid', $request->user()->id)->firstOrFail();
+        $tests   = Test::where('user_id', $school->uid)
+            ->where('class_id', $student->sectionName->class)
+            ->whereRaw('DATE(date) >= ?', [date('Y-m-d')])
+            ->get();
+
+        if(!$tests->isEmpty())
+        {
+            foreach($tests as $key => $test)
+            {
+                $tests[$key]->attempted = TestResult::where('test_id', $test->id)->where('user_id', $request->user()->id)->count();
+            }
+        }
+        
+        return response()->json($tests);
+    } 
     /**
      * Display a listing of the resource.
      *
@@ -22,9 +44,15 @@ class TestController extends Controller
         if (!empty($request->s)) {
             $s = $request->s;
             $query->where(function ($q) use ($s) {
-                $q->where('title', 'LIKE', "%{$s}%")
-                ->orWhere('description', 'LIKE', "%{$s}%");
+                $q->where('name', 'LIKE', "%{$s}%")
+                ->orWhere('duration', 'LIKE', "%{$s}%")
+                ->orWhere('total_questions', 'LIKE', "%{$s}%");
             });
+        }
+
+        if(!empty($request->result) && $request->result === 'yes')
+        {
+            $query->whereHas('test_result');
         }
 
         $tests = $query->paginate($limit);
@@ -67,9 +95,23 @@ class TestController extends Controller
      * @param  \App\Model\Test  $test
      * @return \Illuminate\Http\Response
      */
-    public function show($school, Test $test)
+    public function show(Request $request, $school, $test_id)
     {
-        return response()->json($test);
+        $data = [];
+        
+        $user_id      = $request->user()->id;
+        $data['user'] = User::with(['studentData', 'media'])->find($user_id);
+        $data['test'] = Test::with(['subject', 'instruction', 'test_questions', 'test_users'])->findOrFail($test_id);
+
+        if (!empty($data['test']->test_users))
+        {
+            foreach($data['test']->test_users as $key => $user)
+            {
+                
+            }
+        }
+
+        return response()->json($data);
     }
 
     /**
