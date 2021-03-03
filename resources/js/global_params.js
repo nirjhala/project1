037ea7d69187e1792e883db74545b4488e458e75
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2021-03-03 11:02:42 +0530

    reset

--- resources/js/global_params.js
+++ resources/js/global_params.js
@@ -1,11 +1,11 @@
 export const global_params = {
     http: 'http://',
-    suffixUrl: '.dossierbox.com/',
+    suffixUrl: '.suncitytechno.com/school-management/',
     baseURL: '/school-management/',
     apiBaseUrl: '/school-management/api/',
-    fullApiUrl: 'http://dossierbox.com/api/',
-    registerUrl: 'http://acc.dossierbox.com/',
+    fullApiUrl: 'http://suncitytechno.com/school-management/api/',
+    registerUrl: 'http://acc.suncitytechno.com/school-management/',
     landingBase: 'http://localhost/school-landing/',
-    siteUrl: 'http://dossierbox.com/',
+    siteUrl: 'http://suncitytechno.com/school-management/',
     onlineClassURL: 'http://localhost:3000/'
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,10 +12,10 @@ const mix = require('laravel-mix');
  */
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
-// mix.setResourceRoot('/public')
+mix.setResourceRoot('/public')
 mix.webpackConfig({
     output: {
-        publicPath: '/',
+        publicPath: '/school-management/',
         chunkFilename: 'js/chunks/[name].js',
     }
 });
